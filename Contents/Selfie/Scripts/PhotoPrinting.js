cc731f9685e09fe95fb9cb94fd0e60d5d87b38d8
tamoorkayani <tamoorkayani@gmail.com>
2020-02-21 23:29:47 +0500

    Update

--- Contents/Selfie/Scripts/PhotoPrinting.js
+++ Contents/Selfie/Scripts/PhotoPrinting.js
@@ -226,7 +226,7 @@ function SendEmail()
     var tempEmail=message;
     //var addr = "paul@robotaisolutions.com";
     var subject = "Your robot selfie";
-    var body = "Thank you for attending the NAMR DAS All staff offsite 2019! Here is your photo from our robot.";
+    var body = "Thank you for stopping by Diesel Laptops at TMC20.  Here is your photo from our robot. Have a great day! Diesel Laptops www.diesellaptops.com";
     //var mail_server = "smtp.gmail.com";
     //var mail_addr_sender = "ict.muscatairport@gmail.com";
     //var pswd = "123456789K";
@@ -261,7 +261,7 @@ function SendEmail()
         data: { 
             'fileNAME':tempfilename,
            'email':tempEmail,
-           'apID':'McKesson'
+           'apID':'DieselLaptops'
         }
       }).done(function(response) {
         console.log('saved: ' + response); 
